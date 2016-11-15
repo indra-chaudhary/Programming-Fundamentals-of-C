ffc91743756fc469a9194dc96994aa33212a62e5
indra-chaudhary <theindra11@gmail.com>
2016-11-15 10:12:37 +0545

    Add files via upload

---
+++ forLoop.c
@@ -0,0 +1,25 @@
+/*
+StudentName: Indra Kumar Chaudhary.
+Subject: Programming Fundamentals.
+Roll No:
+Lab No: 14.
+Program: Write a C program to print your name 10 times.
+Date:15-11-2016.
+
+*/
+
+#include<stdio.h>
+
+
+int main(){
+
+
+
+for( int i = 1; i<=10; i++){
+	
+	printf("%d. Indra Kumar Chaudhary\n",i);
+}
+
+
+return 0;
+}
